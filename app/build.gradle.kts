0fb974fb858162fa754a3dc093d92e5d3ab74562
james <james@bubbleye.com>
2024-01-11 15:25:28 +0800

    Bump versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,19 +49,19 @@ android {
 
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2023.08.00")
-    implementation(composeBom)
+    implementation("androidx.compose:compose-bom:2023.10.01")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.compose.ui:ui:1.5.3")
-    implementation("androidx.compose.foundation:foundation-layout-android:1.5.3")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.foundation:foundation-layout-android:1.5.4")
+    implementation("androidx.activity:activity-compose:1.8.2")
     // Android Studio Preview support
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
     // Material
-    implementation("androidx.compose.material:material:1.5.3")
+    implementation("androidx.compose.material:material:1.5.4")
     //implementation("androidx.compose.material3:material3:1.1.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
 
     // Glance
     implementation("androidx.glance:glance-appwidget:1.0.0")
@@ -69,9 +69,9 @@ dependencies {
     implementation("androidx.glance:glance:1.0.0")
 
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
