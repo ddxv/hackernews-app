80a0d595af5294a2e1f05e02f0382dd531725081
James O'Claire <jamesoclaire@gmail.com>
2024-11-18 14:42:51 +0800

    Bumped targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.thirdgate.hackernews"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 10
         versionName = "1.0"
 
