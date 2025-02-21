2d2212612bc70cd124d06bf715baa298243580f8
James O'Claire <jamesoclaire@gmail.com>
2025-02-21 13:46:50 +0800

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.thirdgate.hackernews"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
+        versionCode = 12
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
